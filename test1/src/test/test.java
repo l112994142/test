e5281163ce75ce3f3435d5acd2c1d5f3a0212582
lihao <15662029002@163.com>
2024-02-06 18:42:43 +0800

    tijiao ceshi

--- test1/src/test/test.java
+++ test1/src/test/test.java
@@ -5,7 +5,7 @@ public class test {
         int answer2 = testMethod.test2(6, 2,100);
         double answer3 = testMethod.test3(6, 2,100);
         double answer4 = testMethod.test4(6, 2,100);
-        System.out.println("苹果买"+answer1);
+        System.out.println(answer1);
         System.out.println(answer2);
         System.out.println(answer3);
         System.out.println(answer4);
